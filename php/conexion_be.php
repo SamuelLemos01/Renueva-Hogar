bdaec657e738367f109e513cb7f9520146831ed0
Samuel Lemos <samuellemos907@gmail.com>
2025-04-08 15:26:40 -0500

    Base de datos arreglada

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,15 +1,11 @@
 <?php
 
-$servername = "localhost";
-$username = "senadsog_admin_tienda";
-$password = "S@muel2131";
-$dbname = "senadsog_renueva-hogar";
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
 
+    // if($conexion) {
+    //     echo "Conectado a la base de datos";
+    // } else {
+    //     echo "No se pudo conectar a la base de datos";
+    // }
 
-$conexion = mysqli_connect($servername, $username, $password, $dbname);
-
-
-if (!$conexion) {
-    die("Conexión fallida: " . mysqli_connect_error());
-}
 ?>
