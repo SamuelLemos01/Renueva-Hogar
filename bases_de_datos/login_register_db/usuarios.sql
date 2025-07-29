bf92f4eb70b5f3cba413176d68fcdea3da8bd14c
Samuel Lemos <samuellemos907@gmail.com>
2025-07-28 21:38:25 -0500

    bases de datos añadidas al proyecto

--- bases_de_datos/login_register_db/usuarios.sql
+++ bases_de_datos/login_register_db/usuarios.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-07-2025 a las 18:31:58
+-- Tiempo de generación: 29-07-2025 a las 04:37:27
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `login`
+-- Base de datos: `login_register_db`
 --
 
 -- --------------------------------------------------------
@@ -29,17 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `usuario` varchar(250) NOT NULL,
-  `contraseña` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nombre_completo` varchar(50) NOT NULL,
+  `telefono` varchar(11) NOT NULL,
+  `tipo_documento` varchar(255) NOT NULL,
+  `documento` int(20) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contrasena` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`) VALUES
-(1, 'Jacob Rl', '12345'),
-(2, '', '');
+INSERT INTO `usuarios` (`id`, `nombre_completo`, `telefono`, `tipo_documento`, `documento`, `direccion`, `correo`, `contrasena`) VALUES
+(6, 'samuel lemos', '3242838039', 'CC', 0, 'calle', 'samuellemos907@gmail.com', '123'),
+(7, 'josue', '123456', 'TI', 111, 'calle', 'josue@gmail.com', '123'),
+(8, 'samuel lemos', '123456789', 'CC', 0, 'carrera', 'samuel@gmail.com', '123'),
+(9, 'samuel lemos', '123456789', 'CC', 0, 'carrera', 'samuel@gmail.com', '123');
 
 --
 -- Índices para tablas volcadas
@@ -59,7 +66,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- bases_de_datos/user_admin/usuarios.sql
+++ bases_de_datos/user_admin/usuarios.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-02-2025 a las 02:32:04
+-- Tiempo de generación: 29-07-2025 a las 04:36:39
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `login_register_db`
+-- Base de datos: `user_admin`
 --
 
 -- --------------------------------------------------------
@@ -29,22 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre_completo` varchar(50) NOT NULL,
-  `telefono` varchar(11) NOT NULL,
-  `direccion` varchar(50) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `contrasena` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `usuario` varchar(250) NOT NULL,
+  `contraseña` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre_completo`, `telefono`, `direccion`, `correo`, `contrasena`) VALUES
-(1, 'Samuel Lemos', '3242838039', 'calle 8a #11-63', 'samuellemos907@gmail.com', '123'),
-(2, 'Jacob Rivas', '4240', 'calle 8a #11-63', 'samuellemos907@gmail.com', '123'),
-(3, 'Samuel Lemos', '3242838039', '37, Agrado, Huila, Colombia', 'samuellemos907@gmail.com', '123'),
-(4, 'Samuel Lemos', '3242838039', '37, Agrado, Huila, Colombia', 'samuellemos907@gmail.com', '123');
+INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`) VALUES
+(1, 'Jacob Rl', '12345'),
+(2, 'Samuel', '123'),
+(3, 'Samuel', '123');
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +60,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
